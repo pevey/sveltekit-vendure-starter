aa074c7ac66ce14da6a56228709dd43f8b5f8f47
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2024-03-07 12:28:47 -0600

    home page placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import MetaTags from '$lib/components/MetaTags.svelte'
-
 </script>
 <MetaTags />
+<section class="flex items-center h-200 min-h-[60vh] p-8">
+	<div class="my-auto mx-auto">
+		<h1 class="text-2xl">Your Own Custom Home Page</h1>
+	</div>
+</section>
